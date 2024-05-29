5e44be8b12290ffc1e3c3aa471175d4867769aff
Thanhha2571 <letranthanhha25072001@gmail.com>
2024-05-29 23:16:33 +0700

    fix sth

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -107,7 +107,7 @@ const Navigation = () => {
                             </div>
                         </li>
                         {scheduleMenu && (<ul className="flex flex-col gap-3 list-none">
-                            <a to="/working-schedule">
+                            <a href="/working-schedule">
                                 <li className='px-8 py-2'>
                                     <span className="flex flex-row justify-center items-center gap-4">
                                         <img src={DotIcon} alt="" className='w-3 h-3' />
